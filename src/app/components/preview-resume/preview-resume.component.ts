d4c337d190762081e16180fb5b1aae7f38c2084f
zac-dodol <akmalhazimothman@hotmail.com>
2023-12-19 22:20:57 +0800

    adding more details into the form and the preview

--- src/app/components/preview-resume/preview-resume.component.ts
+++ src/app/components/preview-resume/preview-resume.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -6,15 +6,19 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './preview-resume.component.html',
   styleUrls: ['./preview-resume.component.scss'],
 })
-export class PreviewResumeComponent {
+export class PreviewResumeComponent implements OnInit {
   resumeData: any;
   selectedTemplate: string = 'template1'; // Default template
 
   constructor(private route: ActivatedRoute) {
-    // Retrieve data and selected template passed from the form component
-    this.route.paramMap.subscribe((params) => {
-      this.resumeData = JSON.parse(params.get('data') || '{}');
-      this.selectedTemplate = params.get('template') || 'template1';
+    // Retrieve data and selected template from queryParams
+    this.route.queryParams.subscribe((params) => {
+      this.resumeData = JSON.parse(params['data'] || '{}');
+      this.selectedTemplate = params['template'] || 'template1';
     });
+  }
+
+  ngOnInit(): void {
+    console.log(this.resumeData);
   }
 }
--- src/app/components/resume-form/resume-form.component.ts
+++ src/app/components/resume-form/resume-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,16 +7,81 @@ import { Router } from '@angular/router';
   templateUrl: './resume-form.component.html',
   styleUrls: ['./resume-form.component.scss'],
 })
-export class ResumeFormComponent {
+export class ResumeFormComponent implements OnInit {
+  resumeForm!: FormGroup;
   resumeData: any = {};
 
-  constructor(private router: Router) {}
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.resumeForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', Validators.pattern('^[0-9]{3}-[0-9]{3}-[0-9]{4}$')],
+      address: [''],
+      experiences: this.fb.array([]),
+      education: this.fb.array([]),
+      certifications: this.fb.array([]),
+      description: [''],
+      references: this.fb.array([]),
+      linkedin: [''],
+      skills: this.fb.array([]),
+    });
+  }
+
+  get experiences() {
+    return this.resumeForm.get('experiences') as FormArray;
+  }
+
+  get education() {
+    return this.resumeForm.get('education') as FormArray;
+  }
+
+  get certifications() {
+    return this.resumeForm.get('certifications') as FormArray;
+  }
+
+  get references() {
+    return this.resumeForm.get('references') as FormArray;
+  }
+
+  get skills() {
+    return this.resumeForm.get('skills') as FormArray;
+  }
+
+  addExperience() {
+    this.experiences.push(
+      this.fb.group({
+        title: ['', Validators.required],
+        company: [''],
+        location: [''],
+        startDate: [''],
+        endDate: [''],
+        description: [''],
+      })
+    );
+  }
+
+  removeExperience(index: number) {
+    this.experiences.removeAt(index);
+  }
+
+  // Add similar functions for education, certifications, references, and skills
 
   submitForm() {
+    console.log('Form values:', this.resumeForm.value);
+
     // Add any additional form submission logic here
-    this.router.navigate([
-      '/preview-resume',
-      { data: JSON.stringify(this.resumeData) },
-    ]);
+    this.router.navigate(['/preview-resume'], {
+      queryParams: {
+        data: JSON.stringify(this.resumeForm.value),
+        template: 'template1', // Adjust the template value as needed
+      },
+    });
   }
 }
